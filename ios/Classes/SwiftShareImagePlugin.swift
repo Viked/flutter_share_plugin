0d17ab6eb82bd1894e5720a3698ba5acbe3e5802
Yevgenii Shein <initbin@gmail.com>
2018-08-05 00:09:24 +0300

    Added path and iOS share

--- ios/Classes/SwiftShareImagePlugin.swift
+++ ios/Classes/SwiftShareImagePlugin.swift
@@ -2,15 +2,36 @@ import Flutter
 import UIKit
     
 public class SwiftShareImagePlugin: NSObject, FlutterPlugin {
+
+    static let SHARE_CHANNEL = "channel:app.bestsalary.shareimageplugin/share_image_plugin"
+    static let SHARE_IMAGE = "shareImage"
+    static let IMAGE = "image"
+    static let TEXT = "text"
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "share_image_plugin", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: SHARE_CHANNEL, binaryMessenger: registrar.messenger())
     let instance = SwiftShareImagePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
-
 
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    if(call.method.elementsEqual(SHARE_IMAGE)){
+        let arguments = call.arguments as? NSDictionary
+       let shareImagePath = arguments![IMAGE] as? String
+        
+        let imageURL = URL(shareImagePath)
+        let image    = UIImage(contentsOfFile: imageURL.path)
+        share(image)
+    }
     result("iOS " + UIDevice.current.systemVersion)
   }
+    
+    func share(shareImage:UIImage){
+        var img: UIImage = currentPhoto!
+        var shareItems:Array = [img]
+        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+        activityViewController.excludedActivityTypes = [UIActivityTypePrint, UIActivityTypePostToWeibo, UIActivityTypeCopyToPasteboard, UIActivityTypeAddToReadingList, UIActivityTypePostToVimeo]
+        self.presentViewController(activityViewController, animated: true, completion: nil)
+    }
 }
--- lib/share_image_plugin.dart
+++ lib/share_image_plugin.dart
@@ -1,13 +1,24 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+const SHARE_CHANNEL =
+    "channel:app.bestsalary.shareimageplugin/share_image_plugin";
+const SHARE_IMAGE = "shareImage";
+const IMAGE = "image";
+const TEXT = "text";
 
 class ShareImagePlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('share_image_plugin');
+  static const MethodChannel _channel = const MethodChannel(SHARE_CHANNEL);
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<void> shareImage(String text, File image) async {
+    final tempDir = await getTemporaryDirectory();
+
+    File file = await image.copy('$tempDir/${image.path.split('/').last}');
+
+    await _channel.invokeMethod(SHARE_IMAGE,
+        <String, dynamic>{TEXT: text, IMAGE: file.path.split('/').last});
   }
 }
