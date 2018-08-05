6f40f2d9dbd6a529caf160f5db57fa9363e10a00
Yevgenii Shein <initbin@gmail.com>
2018-08-05 11:12:25 +0300

    Removed path provider

--- lib/share_image_plugin.dart
+++ lib/share_image_plugin.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
 
 const SHARE_CHANNEL =
     "channel:app.bestsalary.shareimageplugin/share_image_plugin";
@@ -14,11 +13,7 @@ class ShareImagePlugin {
   static const MethodChannel _channel = const MethodChannel(SHARE_CHANNEL);
 
   static Future<void> shareImage(String text, File image) async {
-    final tempDir = await getTemporaryDirectory();
-
-    File file = await image.copy('$tempDir/${image.path.split('/').last}');
-
     await _channel.invokeMethod(SHARE_IMAGE,
-        <String, dynamic>{TEXT: text, IMAGE: file.path.split('/').last});
+        <String, dynamic>{TEXT: text, IMAGE: image.path.split('/').last});
   }
 }
