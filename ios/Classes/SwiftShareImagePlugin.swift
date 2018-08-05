fbf7e5661f2ffc1cdd7b3decab3e4efe0cc8d7d0
Yevgenii Shein <initbin@gmail.com>
2018-08-05 12:40:29 +0300

    Fixed swift plugin

--- ios/Classes/SwiftShareImagePlugin.swift
+++ ios/Classes/SwiftShareImagePlugin.swift
@@ -6,19 +6,18 @@ public class SwiftShareImagePlugin: NSObject, FlutterPlugin {
     static let SHARE_CHANNEL = "channel:app.bestsalary.shareimageplugin/share_image_plugin"
     static let SHARE_IMAGE = "shareImage"
     static let IMAGE = "image"
-    static let TEXT = "text"
     
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: SHARE_CHANNEL, binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: SwiftShareImagePlugin.SHARE_CHANNEL, binaryMessenger: registrar.messenger())
     let instance = SwiftShareImagePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
 
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    if(call.method.elementsEqual(SHARE_IMAGE)){
+    if(call.method.elementsEqual(SwiftShareImagePlugin.SHARE_IMAGE)){
         let arguments = call.arguments as? NSDictionary
-       let shareImagePath = arguments![IMAGE] as? String
+       let shareImagePath = arguments![SwiftShareImagePlugin.IMAGE] as? String
         
         let imageURL = URL(shareImagePath)
         let image    = UIImage(contentsOfFile: imageURL.path)
@@ -28,10 +27,10 @@ public class SwiftShareImagePlugin: NSObject, FlutterPlugin {
   }
     
     func share(shareImage:UIImage){
-        var img: UIImage = currentPhoto!
+        var img: UIImage = shareImage!
         var shareItems:Array = [img]
         let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
-        activityViewController.excludedActivityTypes = [UIActivityTypePrint, UIActivityTypePostToWeibo, UIActivityTypeCopyToPasteboard, UIActivityTypeAddToReadingList, UIActivityTypePostToVimeo]
-        self.presentViewController(activityViewController, animated: true, completion: nil)
+        activityViewController.excludedActivityTypes = [UIActivityType.print, UIActivityType.postToWeibo, UIActivityType.copyToPasteboard, UIActivityType.addToReadingList, UIActivityType.postToVimeo]
+        self.present(activityViewController, animated: true, completion: nil)
     }
 }
